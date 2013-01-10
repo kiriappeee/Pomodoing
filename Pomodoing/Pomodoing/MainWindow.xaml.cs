fba869f55a4c4f95832eeed84537b718d79ef28d
kiriappeee <eddyrox1@hotmail.com>
2013-01-10 06:51:58 +0530

    random commit

--- Pomodoing/Pomodoing/MainWindow.xaml.cs
+++ Pomodoing/Pomodoing/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pomodoing
 {
@@ -17,11 +18,41 @@ namespace Pomodoing
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private DispatcherTimer Timer;
+		private bool PomodoroStarted; //
+		private int timeMinutes;
+        private TimeSpan timeRemaining;
 		public MainWindow()
 		{
 			this.InitializeComponent();
+			
+			Timer = new DispatcherTimer();
+			Timer.Tick += new EventHandler(Timer_Tick);
+			Timer.Interval = new TimeSpan(0, 0, 1);
+			timeMinutes = 25;
+            timeRemaining = new TimeSpan(0, 25, 0);
+		}
 
-			// Insert code required on object creation below this point.
+		void Timer_Tick(object sender, EventArgs e)
+		{
+			//logic to handle the display of numbers here. Maybe binding the variables is a good idea but then again is over optimization for now. 
+            timeRemaining = timeRemaining.Subtract(TimeSpan.FromSeconds(1.00));
+            lblMinutes.Content = timeRemaining.Minutes;
+            lblSeconds.Content = timeRemaining.Seconds.ToString(;
+		}
+
+		private void btnStartStop_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			if (PomodoroStarted)
+			{
+
+			}
+			else
+			{
+				Timer.Start();    
+			}
+			
+				
 		}
 	}
 }
